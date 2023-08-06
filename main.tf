941775744a19442cd5ce47c7f56d48e4d2b3288a
Kenneth Myers <kennjamesmyers@gmail.com>
2023-08-06 13:06:29 -0400

    modularized dynamo calls

--- main.tf
+++ main.tf
@@ -1,63 +1,20 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+  required_providers {}
 
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "us-east-2"
+module "dynamodb-table-rising-dev" {
+  source = "./modules/dynamodb"
+
+  env = "dev"
+  reddit_view = "rising"
 }
 
-variable "table_fields" {
-  type = map(string)
-  default = {
-    loadDateUTC    = "S"
-    loadTimeUTC    = "S"
-    loadTSUTC      = "S"
-    postId         = "S"
-  }
+module "dynamodb-table-hot-dev" {
+  source = "./modules/dynamodb"
+
+  env = "dev"
+  reddit_view = "hot"
 }
 
-resource "aws_dynamodb_table" "hot-dynamodb-table-dev" {
-  name           = "hot-dev"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 6
-  write_capacity = 1
-  hash_key       = "postId"
-  range_key      = "loadTSUTC"
-
-  # dynamic blocks https://developer.hashicorp.com/terraform/language/expressions/dynamic-blocks
-  dynamic "attribute" {
-    for_each = var.table_fields
-    content {
-      name   = attribute.key
-      type = attribute.value
-    }
-  }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
-  global_secondary_index {
-    name               = "byLoadDate"
-    hash_key           = "loadDateUTC"
-    range_key          = "loadTimeUTC"
-    write_capacity     = 6
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
-  }
-
-  tags = {
-    Name        = "dynamodb-table-hot"
-    Environment = "dev"
-    Project     = "viral-reddit-posts"
-  }
-}
---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "The name of the environment ie dev/tst/stg/prd"
+  type        = string
+}
+
+variable "reddit_view" {
+  description = "The name of the reddit view data is pulled from ie hot/rising/new"
+  type        = string
+}
